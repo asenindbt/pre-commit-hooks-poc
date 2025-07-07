7940449ee68bcdfc86b87b4473d0366babf500d3
Aishah Senin <aishah.senin@businessandtrade.gov.uk>
2025-07-07 11:46:57 +0100

    added print statements

--- pre_commit_hooks/run_org_hooks/main.py
+++ pre_commit_hooks/run_org_hooks/main.py
@@ -7,6 +7,10 @@ HERE = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     cfg = os.path.join(HERE, 'orghooks.yaml')
+
+    print(f'cfg: {cfg}')
+    print(f'sys.argv[1]: {sys.argv[1]}')
+
     cmd = ['pre-commit', 'run', '--config', cfg, '--files'] + sys.argv[1:]
     os.execvp(cmd[0], cmd)
 
