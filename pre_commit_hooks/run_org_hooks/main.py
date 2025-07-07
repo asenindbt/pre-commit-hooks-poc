994c7c7ceb39d6a1cb978d256d858133cf5f61cf
Aishah Senin <aishah.senin@businessandtrade.gov.uk>
2025-07-07 11:56:45 +0100

    test

--- pre_commit_hooks/run_org_hooks/main.py
+++ pre_commit_hooks/run_org_hooks/main.py
@@ -6,12 +6,10 @@ HERE = os.path.dirname(os.path.realpath(__file__))
 
 
 def main():
-    cfg = os.path.join(HERE, 'orghooks.yaml')
+    # cfg = os.path.join(HERE, 'orghooks.yaml')
 
-    print(f'cfg: {cfg}')
-    print(f'sys.argv[1]: {sys.argv[1]}')
-
-    cmd = ['pre-commit', 'run', '--config', cfg, '--files'] + sys.argv[1:]
+    cmd = ['pre-commit', 'run']
+    # cmd = ['pre-commit', 'run', '--config', cfg, '--files'] + sys.argv[1:]
     os.execvp(cmd[0], cmd)
 
 
